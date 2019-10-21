d6d449109fb09d75e63bc9b574317d2421c4ce2c
mHaleta <gm199716@gmail.com>
2019-10-21 22:34:25 +0300

    Update AOS.py

--- AOS.py
+++ AOS.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 
 class ProcessorImitation:
     def __init__(self, n_bits):
@@ -13,6 +14,14 @@ class ProcessorImitation:
             'PS': 0
         }
         self.n_bits = n_bits
+        self.commands = {
+            'mov': self.mov,
+            'add': self.add,
+            'mod': self.mod
+        }
+        
+    def execute_command(self, command, *args):
+        self.commands[command](*args)
         
     def twos_complement(self, n, bits):
         mask = (1 << bits) - 1
@@ -25,36 +34,39 @@ class ProcessorImitation:
     def output(self):
         frame = pd.DataFrame.from_dict(self.d, orient='index', columns=[''])
         print(frame)
-        print()
     
     def set_ir(self, ir):
         self.d['IR'] = ir
         self.d['PC'] = self.d['PC'] + 1
-        self.d['TC'] = self.d['TC'] % 2 +1
+        self.d['TC'] = self.d['TC'] % 2 + 1
         self.output()
     
     def mov(self, reg, value):
-        self.d[reg] = self.twos_complement(value, self.n_bits)
+        self.set_ir('mov {}, {}'.format(reg, value))
+        self.d[reg] = self.twos_complement(int(value), self.n_bits)
         self.d['TC'] = self.d['TC'] % 2 + 1
         self.d['PS'] = self.d[reg][2]
         self.output()
         
-    def add(self, reg1, reg2, reg3):
-        self.d[reg3] = self.twos_complement(bin(int(self.d[reg1], 2)) + bin(int(self.d[reg2], 2)), self.n_bits)
+    def add(self, reg1, reg2, reg3='R3'):
+        self.set_ir('add {}, {}'.format(reg1, reg2))
+        self.d[reg3] = self.twos_complement(int(self.d[reg1], 2) + int(self.d[reg2], 2), self.n_bits)
         self.d['TC'] = self.d['TC'] % 2 + 1
         self.d['PS'] = self.d[reg3][2]
         self.output()
         
-    def mod(self, reg1, reg2, reg3):
+    def mod(self, reg1, reg2, reg3='R3'):
+        self.set_ir('mod {}, {}'.format(reg1, reg2))
         self.d[reg3] = self.twos_complement(divmod(int(self.d[reg1], 2), int(self.d[reg2], 2))[1], self.n_bits)
         self.d['TC'] = self.d['TC'] % 2 + 1
         self.d['PS'] = self.d[reg3][2]
         self.output()
+        
+with open("Commands.txt", 'r') as f:
+    commands = []
+    for line in f:
+        commands.append(line.replace('\n', ''))
 
 proc_imit = ProcessorImitation(24)
-proc_imit.set_ir('mov R1, 1256')
-proc_imit.mov('R1', 1256)
-proc_imit.set_ir('mov R2, 317')
-proc_imit.mov('R2', 317)
-proc_imit.set_ir('R1 mod R2')
-proc_imit.mod('R1', 'R2', 'R3')
+for command in commands:
+    proc_imit.execute_command(*re.split(' |, ', command))
